955bc71e4c2b0d61f20f94b5e2689061d15bcbd9
Thierry de Pauw <thierry@thinkinglabs.io>
2017-01-20 15:41:44 +0100

    describe the purpose of mvn-settings.bash

--- .bashrc.d/mvn-settings.bash
+++ .bashrc.d/mvn-settings.bash
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
+# Creates a symbolic link ~/.m2/settings.xml to the settings file to use
+# Allows for switching between different Maven settings when working for
+# different organisations.
 function mvn-settings {
   rm ~/.m2/settings.xml
   ln -s ~/.m2/settings-$1.xml ~/.m2/settings.xml
