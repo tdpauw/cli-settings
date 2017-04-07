f64fc4fdae8c7273b38cf9f6f731cdec911f168b
Thierry de Pauw <thierry@thinkinglabs.io>
2017-04-07 20:58:55 +0200

    configure the git clean prompt with the check character

--- .zshrc.d/20-theme-blinks.zsh
+++ .zshrc.d/20-theme-blinks.zsh
@@ -48,7 +48,7 @@ esac
 ZSH_THEME_GIT_PROMPT_PREFIX=" |%{%B%F{blue}%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%K{${bkg}}%B%F{green}%}|"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}✗%{%f%k%b%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
+ZSH_THEME_GIT_PROMPT_CLEAN=" %{%F{green}%}✓%{%f%k%b%}"
 
 PROMPT='%{%f%k%b%}
 %{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
