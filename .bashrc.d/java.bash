4c1d75c1d533e6bec3e883f324d941f262c32838
Thierry de Pauw <thierry@thinkinglabs.io>
2018-03-23 11:49:17 +0100

    fix the product name in the license header

--- .bashrc.d/java.bash
+++ .bashrc.d/java.bash
@@ -14,6 +14,6 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with cli-settings.  If not, see <http://www.gnu.org/licenses/>.
 
 export JAVA_HOME=$(/usr/libexec/java_home)
