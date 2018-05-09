fbf50c96fcdbcc0c57f1c41415187ea153821196
Thierry de Pauw <thierry@thinkinglabs.io>
2018-05-09 13:33:24 +0200

    set GROOVY_HOME environment variable

---
+++ .bashrc.d/groovy.bash
@@ -0,0 +1,19 @@
+#
+# (C) Copyright 2018 Thierry de Pauw
+#
+# This file is part of cli-settings.
+
+# cli-settings is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cli-settings is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cli-settings.  If not, see <http://www.gnu.org/licenses/>.
+
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
