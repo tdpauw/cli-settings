86bbcefd9908374dcc3b42de429b0c4d01bfd6a8
Thierry de Pauw <thierry@thinkinglabs.io>
2018-11-16 21:31:56 +0100

    configure jenv

--- .bashrc.d/java.bash
+++ .bashrc.d/java.bash
@@ -16,4 +16,5 @@
 # You should have received a copy of the GNU General Public License
 # along with cli-settings.  If not, see <http://www.gnu.org/licenses/>.
 
-export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
