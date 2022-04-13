fd58efa6e481938b32c36bd35c46b34fdbae401e
Thierry de Pauw <thierry@thinkinglabs.io>
2022-04-13 10:49:01 +0200

    Move the ruby configurations to zsh and homebrew for m1

--- .zshrc.d/ruby.zsh
+++ .zshrc.d/ruby.zsh
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
+source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
 
+# auto-switching of Rubies specified by .ruby-version files
+source /opt/homebrew/opt/chruby/share/chruby/auto.sh
