23ceee615da732b4684879aaa684869da405df75
Thierry de Pauw <thierry@thinkinglabs.io>
2025-02-14 14:29:36 +0100

    Setup darwin-nix

---
+++ etc/nix-darwin/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.devenv
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      security.pam.enableSudoTouchIdAuth = true;
+      system.defaults = {
+        # hide dock
+        dock.autohide = true;
+        # do not rearrange spaces based on most the recent use
+        dock.mru-spaces = false;
+    };
+
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#coyote
+    darwinConfigurations."coyote" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
