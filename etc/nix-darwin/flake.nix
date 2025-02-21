6b5368a79dc4436f593ced2d2d5680c613e09afa
Thierry de Pauw <thierry@thinkinglabs.io>
2025-02-21 15:05:10 +0100

    Install editorconfig core using nixos instead of homebrew

--- etc/nix-darwin/flake.nix
+++ etc/nix-darwin/flake.nix
@@ -15,6 +15,7 @@
       environment.systemPackages =
         [ pkgs.devenv
           pkgs.direnv
+          pkgs.editorconfig-core-c
           pkgs.unixtools.watch
           pkgs.fzf
           pkgs.gh
